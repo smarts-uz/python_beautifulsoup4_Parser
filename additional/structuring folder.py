0fe4570cfdc502dc50c443cd4e19d17a50d28c96
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-10-17 12:45:05 +0500

    Add files via upload

--- additional/structuring folder.py
+++ additional/structuring folder.py
@@ -4,22 +4,28 @@ import shutil
 post_title = "form_builder_chips_input | @ Bads | Form Builders | Flutter".split('|') # post title divide to list
 post_title.reverse()  # reverse created list
 
-actual_path = 'D:\\test\\7\\' # path where we will save our data
+# path where we will save our data
+actual_path = 'D:\\test\\11\\'
 
-#loop for creating
-for i in post_title:
-    actual_path = actual_path + i + "\\"
-    os.mkdir(actual_path)
 renamed_file = '\Copied_SmartDust Labs.mp4'
-file_location = 'C:\\Users\\Administrator\\Downloads\\SmartDust Labs.mp4'
-shutil.copyfile(file_location, actual_path + renamed_file)
+#loop for creating
+
+def folder_structure(post_title, actual_path, renamed_file):
+    for i in post_title:
+        actual_path = actual_path + i + "\\"
+        if not os.path.exists(actual_path):
+            os.mkdir(actual_path)
+    file_location = fr'C:\Users\Administrator\Downloads\SmartDust Labs.mp4'
+    shutil.copyfile(file_location, actual_path + renamed_file)
 
 
 def name_reducer(viriable):
-    limit = 100
+    limit = 10
     if len(viriable) > limit:
         x = len(viriable) - limit
-        viriable = viriable.replace(viriable[-x:], '')
+        extension = (viriable[-5:])
+        viriable = viriable.replace(viriable[-x:], '') + extension
+        return viriable
     else:
         pass
 
@@ -30,5 +36,8 @@ def crop_title(): #приминяеться для удаления расшир
         del a[-1] #deleting .mp4
         print(a)
         
-def removing_hashtags(): # убираются хэштеги
-    pass
+def removing_hashtags(target): # remove hashtags
+    target = target.replace("#", " ")
+
+
+folder_structure(post_title, actual_path,name_reducer(renamed_file))
