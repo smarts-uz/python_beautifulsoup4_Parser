ad01abc451564eb709bfc121788148036616f5c9
Sherlock0808 <132607569+Sherlock0808@users.noreply.github.com>
2023-10-18 19:42:41 +0500

    creating folders

--- additional/functions.py
+++ additional/functions.py
@@ -8,7 +8,6 @@ def correct_post_title(post_title):
     third_step = second_step[0]
     first_step[-1] = third_step
     first_step.reverse()
-    print(first_step)
     return first_step
 
 
@@ -17,10 +16,42 @@ def correct_video_title(video_path, post_title):
     if first_step[-1].startswith('/Rec'):
         video_title = '\\' + post_title[-1] + '.mp4'
     else:
-        video_title = first_step[-1]
+        video_title_1 = first_step[-1]
+        first_step = video_title_1.split('/')
+        video_title = '\\' + first_step[-1]
     return video_title
 
 
 def correct_data_title(data_title):
-    year = data_title[6:11]
+    year = data_title[6:10]
     return year
+
+
+def correct_file_location(video_path, data_title):
+    base_dir = r"\\" + "192.168.100.100\SmartTech Learning Group\\"
+    file_directory = base_dir + data_title
+    file_path_list = listdir(file_directory)
+    for i in file_path_list:
+        link = file_directory + '\\' + i
+        video_link = link + '\\' + video_path
+        if os.path.isfile(video_link):
+            return video_link
+
+
+def correct_video_duration(video_duration):
+    result = '-'.join(video_duration.split(':'))
+    return result
+
+
+#  \\192.168.100.100\SmartTech Learning Group\2023\9-8\video_files\«Account» folder of Services.mp4 True
+#  \\192.168.100.100\SmartTech Learning Group\\2023\9-8\video_files\«Account» folder of Services.mp4  false
+
+
+# file_path_list = listdir(r'\\192.168.100.100\SmartTech Learning Group\2021 ')
+# print(file_path_list)
+
+a = '22-42'
+b = 'These methods allow you to write either a single line at a time or write multiple lines to an opened file. While Python allows you to open a file using the open()'
+path = fr'D:\108\Test\\'
+with open(fr'{path}{a}.txt', 'w', encoding='UTF-8') as file:
+    file.write(b)
--- additional/structuring folder.py
+++ additional/structuring folder.py
@@ -3,6 +3,7 @@ import shutil
 import mysql.connector
 import json
 from os import listdir
+from additional.functions import correct_data_title, correct_video_title, correct_post_title, correct_file_location, correct_video_duration
 
 
 def read_mysql():
@@ -16,18 +17,7 @@ def read_mysql():
     myresult = mycursor.fetchall()
     # for i in myresult:
     #     print(i)
-    return myresult
-
-
-def correct_file_location(video_path, data_title):
-    base_dir = r"\\" + "192.168.100.100\SmartTech Learning Group\\"
-    file_directory = base_dir + data_title
-    file_path_list = listdir(file_directory)
-    for i in file_path_list:
-        link = file_directory + '\\' + i
-        video_link = link + '\\' + video_path
-        if os.path.isfile(video_link):
-            return video_link
+    return myresult[0:100]
 
 
 def create_dirs(list_of_data):
@@ -36,60 +26,23 @@ def create_dirs(list_of_data):
         post_title = correct_post_title(item[0])
         video_path = item[1]
         description = item[2]
-        video_duration = item[3]
+        video_duration = correct_video_duration(item[3])
         data_title = correct_data_title(item[4])
         video_title = correct_video_title(video_path, post_title)
         for i in post_title:
-            actual_path = actual_path + i + "\\"
-            if not os.path.exists(actual_path):
-                os.mkdir(actual_path)
-        file_location = correct_file_location(video_path, data_title)
-        shutil.copy2(file_location, actual_path + video_title)
+            actual_path_dir = actual_path + i + "\\"
+            if not os.path.exists(actual_path_dir):
+                os.mkdir(actual_path_dir)
+            file_location = correct_file_location(video_path, data_title)
+            shutil.copy2(file_location, actual_path_dir + video_title)
+            with open(f'{actual_path_dir}{video_duration}.txt', 'x', encoding='UTF-8') as file:
+                try:
+                    file.write(description)
+                except:
+                    file.write('There is no description')
 
 
 data = read_mysql()
-create_dirs(data)
+#create_dirs(data)
 
 
-#  correct_file_location(r'video_files\«Account» folder of Services.mp4', '2023')
-
-#  "\\192.168.100.100\SmartTech Learning Group\2023\9-8\video_files\«Account» folder of Services.mp4"
-#  \\192.168.100.100\SmartTech Learning Group\\2023\9-8\video_files\«Account» folder of Services.mp4
-
-# my_path = listdir(r'\\192.168.100.100\SmartTech Learning Group\\2021') # \\192.168.100.100\SmartTech Learning Group\2021
-# for i in my_path:
-#     print(i)
-
-
-# path where we will save our data
-# actual_path = 'D:\\108\\Test\\'
-#
-#
-# renamed_file = '\\Succes_2.mp4'
-#
-# post_title = "Uztelecom | circleci.com| CICD".split('|')  # post title divide to list
-# post_title.reverse()
-
-
-def folder_structure(post_title, actual_path, renamed_file):
-    for i in post_title:
-        actual_path = actual_path + i + "\\"
-        if not os.path.exists(actual_path):
-            os.mkdir(actual_path)
-    file_location = fr'\\192.168.100.100\SmartTech Learning Group\2021\2021-9\video_files\flutter map + polyline.mp4'
-    shutil.copy2(file_location, actual_path + renamed_file)
-
-
-def name_reducer(viriable):
-    limit = 10
-    if len(viriable) > limit:
-        x = len(viriable) - limit
-        extension = (viriable[-5:])
-        viriable = viriable.replace(viriable[-x:], '') + extension
-        print(viriable)
-        return viriable
-    else:
-        pass
-
-
-#folder_structure(post_title, actual_path, name_reducer(renamed_file))
