91fe5c757468b1e3ff0b9ba6fa61a12451cf6892
HunterZwer <117033000+HunterZwer@users.noreply.github.com>
2023-10-24 19:08:45 +0500

    Add files via upload

--- additional/extract_urls_from_mhtml.py
+++ additional/extract_urls_from_mhtml.py
@@ -1,30 +1,33 @@
 from bs4 import BeautifulSoup
 import os
+import requests
 from dotenv import load_dotenv
 load_dotenv()
-
 file_path = os.getenv('paz')
 print(file_path)
-# def extract_urls_from_mhtml(file_path):
-#     with open(file_path, 'r') as file:
-#         mhtml_content = file.read()
-#
-#     soup = BeautifulSoup(mhtml_content, 'lxml')
-#     urls = [a['href'] for a in soup.find_all('a', href=True)]
-#
-#     return urls
-#
-# # Replace 'your_file.mhtml' with the actual file path
-# mhtml_file = file_path+'Python open() Function.mhtml'
-# urls = extract_urls_from_mhtml(mhtml_file)
-#
-# for url in urls:
-#     print(url)
 def extract_urls_from_mhtml(file_path):
-    mhtml_file = file_path+'how many files in folder python script - Search.mhtml'
+    mhtml_file = file_path+"python - ImportError_ No module named 'MySQL' - Stack Overflow.mhtml"
     with open(mhtml_file, 'r') as file:
         mhtml_content = file.readlines()
         url = mhtml_content[1][27:]
         return url
 
+
+path = "where to save url file"
+url = extract_urls_from_mhtml(file_path)
+encoding  ="""
+[{000214A0-0000-0000-C000-000000000046}]
+Prop3=19,11
+[InternetShortcut]
+IDList=
+URL="""+url+"""
+IconIndex=13
+HotKey=0
+IconFile=C:\Windows\System32\SHELL32.dll
+"""
+with open(path+'tes.url','w') as file:
+    file.write(encoding)
+
+
 print(extract_urls_from_mhtml(file_path))
+
